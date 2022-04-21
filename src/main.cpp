303181d64a32eafcded0ee44394eb3592c1deedf
Evgeniy <ternovskiya@mail.ru>
2022-04-21 23:58:46 +0400

    Added regiter processing & step over bp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,68 @@
 #include <unistd.h>
 #include <sstream>
 #include <iostream>
+#include <iomanip>
 
 #include "linenoise/linenoise.h"
 
 #include "debugger.h"
+#include "registers.h"
 
 using namespace test;
+
+uint64_t debugger::read_memory(uint64_t address) {
+    return ptrace(PTRACE_PEEKDATA, m_pid, address, nullptr);
+}
+
+void debugger::write_memory(uint64_t address, uint64_t value) {
+    ptrace(PTRACE_POKEDATA, m_pid, address, value);
+}
+
+uint64_t debugger::get_pc() {
+    return get_register_value(m_pid, reg::rip);
+}
+
+void debugger::set_pc(uint64_t pc) {
+    set_register_value(m_pid, reg::rip, pc);
+}
+
+void debugger::step_over_breakpoint() {
+    // - 1 because execution will go past the breakpoint
+    auto possible_breakpoint_location = get_pc() - 1;
+
+    if (m_breakpoints.count(possible_breakpoint_location)) {
+        auto& bp = m_breakpoints[possible_breakpoint_location];
+        if (bp.is_enabled()) {
+            auto previous_instruction_address = possible_breakpoint_location;
+            set_pc(previous_instruction_address);
+
+            bp.disable();
+            ptrace(PTRACE_SINGLESTEP, m_pid, nullptr, nullptr);
+            wait_for_signal();
+            bp.enable();
+        }
+    }
+}
+
+void debugger::wait_for_signal() {
+    int wait_status;
+    auto options = 0;
+    waitpid(m_pid, &wait_status, options);
+}
+
+
+void debugger::continue_execution() {
+    step_over_breakpoint();
+    ptrace(PTRACE_CONT, m_pid, nullptr, nullptr);
+    wait_for_signal();
+}
+
+void debugger::dump_registers() {
+    for (const auto& rd : g_register_descriptors) {
+        std::cout << rd.name << " 0x"
+                  << std::setfill('0') << std::setw(16) << std::hex << get_register_value(m_pid, rd.r) << std::endl;
+    }
+}
 
 std::vector<std::string> split(const std::string &s, char delimiter) {
     std::vector<std::string> out{};
@@ -40,6 +96,29 @@ void debugger::handle_command(const std::string& line) {
         std::string addr {args[1], 2}; //naively assume that the user has written 0xADDRESS
         set_breakpoint_at_address(std::stol(addr, 0, 16));
     }
+    else if (is_prefix(command, "register")) {
+        if (is_prefix(args[1], "dump")) {
+            dump_registers();
+        }
+        else if (is_prefix(args[1], "read")) {
+            std::cout << get_register_value(m_pid, get_register_from_name(args[2])) << std::endl;
+        }
+        else if (is_prefix(args[1], "write")) {
+            std::string val {args[3], 2}; //assume 0xVAL
+            set_register_value(m_pid, get_register_from_name(args[2]), std::stol(val, 0, 16));
+        }
+    }
+    else if(is_prefix(command, "memory")) {
+        std::string addr {args[2], 2}; //assume 0xADDRESS
+
+        if (is_prefix(args[1], "read")) {
+            std::cout << std::hex << read_memory(std::stol(addr, 0, 16)) << std::endl;
+        }
+        if (is_prefix(args[1], "write")) {
+            std::string val {args[3], 2}; //assume 0xVAL
+            write_memory(std::stol(addr, 0, 16), std::stol(val, 0, 16));
+        }
+    }
     else {
         std::cerr << "Unknown command\n";
     }
@@ -53,9 +132,7 @@ void debugger::set_breakpoint_at_address(std::intptr_t addr) {
 }
 
 void debugger::run() {
-    int wait_status;
-    auto options = 0;
-    waitpid(m_pid, &wait_status, options);
+    wait_for_signal();
 
     char* line = nullptr;
     while((line = linenoise("minidbg> ")) != nullptr) {
@@ -63,14 +140,6 @@ void debugger::run() {
         linenoiseHistoryAdd(line);
         linenoiseFree(line);
     }
-}
-
-void debugger::continue_execution() {
-    ptrace(PTRACE_CONT, m_pid, nullptr, nullptr);
-
-    int wait_status;
-    auto options = 0;
-    waitpid(m_pid, &wait_status, options);
 }
 
 void execute_debugee (const std::string& prog_name) {
