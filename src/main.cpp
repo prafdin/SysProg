545bee020474c118ab55ee128d245469b1c4b031
Evgeniy <ternovskiya@mail.ru>
2022-04-27 21:47:32 +0400

    Added parser

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 #include "linenoise/linenoise.h"
 
+#include "parser.h"
 #include "debugger.h"
 #include "registers.h"
 
@@ -151,12 +152,12 @@ void execute_debugee (const std::string& prog_name) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Program name not specified";
-        return -1;
+    ArgParser args(argc, argv);
+    if (!args.parse()) {
+        return 1;
     }
 
-    auto prog = argv[1];
+    auto prog = args.getProgName();
 
     auto pid = fork();
     if (pid == 0) {
