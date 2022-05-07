d0942efa93b3fdb948546c1002cd9f7100098564
Evgeniy <ternovskiya@mail.ru>
2022-05-07 23:51:38 +0400

    Change include libelfin

--- lib/libelfin/elf/elf++.hh
+++ lib/libelfin/elf/elf++.hh
@@ -15,17 +15,12 @@
 
 ELFPP_BEGIN_NAMESPACE
 
-    class elf;
-
-    class loader;
-
-    class section;
-
-    class strtab;
-
-    class symtab;
-
-    class segment;
+class elf;
+class loader;
+class section;
+class strtab;
+class symtab;
+class segment;
 // XXX Audit for binary compatibility
 
 // XXX Segments, other section types
@@ -33,14 +28,14 @@ ELFPP_BEGIN_NAMESPACE
 /**
  * An exception indicating malformed ELF data.
  */
-    class format_error : public std::runtime_error {
-    public:
+class format_error : public std::runtime_error
+{
+public:
         explicit format_error(const std::string &what_arg)
-                : std::runtime_error(what_arg) {}
-
+                : std::runtime_error(what_arg) { }
         explicit format_error(const char *what_arg)
-                : std::runtime_error(what_arg) {}
-    };
+                : std::runtime_error(what_arg) { }
+};
 
 /**
  * An ELF file.
@@ -53,15 +48,16 @@ ELFPP_BEGIN_NAMESPACE
  * section data.  Hence, callers must ensure that the loader passed to
  * this file remains live as long as any such pointer is in use.
  * Keeping any object that can return such a pointer live is
- * sufficieint to keep the loader live.
+ * sufficient to keep the loader live.
  */
-    class elf {
-    public:
+class elf
+{
+public:
         /**
          * Construct an ELF file that is backed by data read from the
          * given loader.
          */
-        explicit elf(const std::shared_ptr <loader> &l);
+        explicit elf(const std::shared_ptr<loader> &l);
 
         /**
          * Construct an ELF file that is initially not valid.  Calling
@@ -69,15 +65,14 @@ ELFPP_BEGIN_NAMESPACE
          * undefined behavior.
          */
         elf() = default;
-
         elf(const elf &o) = default;
-
         elf(elf &&o) = default;
 
-        elf &operator=(const elf &o) = default;
+        elf& operator=(const elf &o) = default;
 
-        bool valid() const {
-            return !!m;
+        bool valid() const
+        {
+                return !!m;
         }
 
         /**
@@ -89,12 +84,12 @@ ELFPP_BEGIN_NAMESPACE
         /**
          * Return the loader used by this file.
          */
-        std::shared_ptr <loader> get_loader() const;
+        std::shared_ptr<loader> get_loader() const;
 
         /**
          * Return the segments in this file.
          */
-        const std::vector <segment> &segments() const;
+        const std::vector<segment> &segments() const;
 
         /**
          * Return the segment at the given index. If no such segment
@@ -105,7 +100,7 @@ ELFPP_BEGIN_NAMESPACE
         /**
          * Return the sections in this file.
          */
-        const std::vector <section> &sections() const;
+        const std::vector<section> &sections() const;
 
         /**
          * Return the section with the specified name. If no such
@@ -119,17 +114,18 @@ ELFPP_BEGIN_NAMESPACE
          */
         const section &get_section(unsigned index) const;
 
-    private:
+private:
         struct impl;
-        std::shared_ptr <impl> m;
-    };
+        std::shared_ptr<impl> m;
+};
 
 /**
  * An interface for loading sections of an ELF file.
  */
-    class loader {
-    public:
-        virtual ~loader() {}
+class loader
+{
+public:
+        virtual ~loader() { }
 
         /**
          * Load the requested file section into memory and return a
@@ -139,26 +135,26 @@ ELFPP_BEGIN_NAMESPACE
          * (including a premature EOF), it must throw an exception.
          */
         virtual const void *load(off_t offset, size_t size) = 0;
-    };
+};
 
 /**
  * An mmap-based loader that maps requested sections on demand.  This
  * will close fd when done, so the caller should dup the file
  * descriptor if it intends to continue using it.
  */
-    std::shared_ptr <loader> create_mmap_loader(int fd);
+std::shared_ptr<loader> create_mmap_loader(int fd);
 
 /**
  * An exception indicating that a section is not of the requested type.
  */
-    class section_type_mismatch : public std::logic_error {
-    public:
+class section_type_mismatch : public std::logic_error
+{
+public:
         explicit section_type_mismatch(const std::string &what_arg)
-                : std::logic_error(what_arg) {}
-
+                : std::logic_error(what_arg) { }
         explicit section_type_mismatch(const char *what_arg)
-                : std::logic_error(what_arg) {}
-    };
+                : std::logic_error(what_arg) { }
+};
 
 /**
  * An ELF segment.
@@ -166,56 +162,56 @@ ELFPP_BEGIN_NAMESPACE
  * This class is internally reference counted and efficiently
  * copyable.
  */
-    class segment {
-    public:
-        /**
-         * Construct a segment that is initially not valid.  Calling
-         * methods other than operator= and valid on this results in
-         * undefined behavior.
-         */
-        segment() {}
+class segment
+{
+public:
+       /**
+        * Construct a segment that is initially not valid.  Calling
+        * methods other than operator= and valid on this results in
+        * undefined behavior.
+        */
+       segment() { }
 
-        segment(const elf &f, const void *hdr);
+       segment(const elf &f, const void *hdr);
+       segment(const segment &o) = default;
+       segment(segment &&o) = default;
 
-        segment(const segment &o) = default;
+       /**
+        * Return true if this segment is valid and corresponds to a
+        * segment in the ELF file.
+        */
+       bool valid() const
+       {
+               return !!m;
+       }
 
-        segment(segment &&o) = default;
+       /**
+        * Return the ELF section header in canonical form (ELF64 in
+        * native byte order).
+        */
+       const Phdr<> &get_hdr() const;
 
-        /**
-         * Return true if this segment is valid and corresponds to a
-         * segment in the ELF file.
-         */
-        bool valid() const {
-            return !!m;
-        }
+       /**
+        * Return this segment's data. The returned buffer will
+        * be file_size() bytes long.
+        */
+       const void *data() const;
 
-        /**
-         * Return the ELF section header in canonical form (ELF64 in
-         * native byte order).
-         */
-        const Phdr<> &get_hdr() const;
+       /**
+        * Return the on disk size of this segment in bytes.
+        */
+       size_t file_size() const;
 
-        /**
-         * Return this segment's data. The returned buffer will
-         * be file_size() bytes long.
-         */
-        const void *data() const;
+       /**
+        * Return the in-memory size of this segment in bytes.
+        * Bytes between file_size() and mem_size() are implicitly zeroes.
+        */
+       size_t mem_size() const;
 
-        /**
-         * Return the on disk size of this segment in bytes.
-         */
-        size_t file_size() const;
-
-        /**
-         * Return the in-memory size of this segment in bytes.
-         * Bytes between file_size() and mem_size() are implicity zeroes.
-         */
-        size_t mem_size() const;
-
-    private:
-        struct impl;
-        std::shared_ptr <impl> m;
-    };
+private:
+       struct impl;
+       std::shared_ptr<impl> m;
+};
 
 /**
  * An ELF section.
@@ -223,27 +219,27 @@ ELFPP_BEGIN_NAMESPACE
  * This class is internally reference counted and efficiently
  * copyable.
  */
-    class section {
-    public:
+class section
+{
+public:
         /**
          * Construct a section that is initially not valid.  Calling
          * methods other than operator= and valid on this results in
          * undefined behavior.
          */
-        section() {}
+        section() { }
 
         section(const elf &f, const void *hdr);
-
         section(const section &o) = default;
-
         section(section &&o) = default;
 
         /**
          * Return true if this section is valid and corresponds to a
          * section in the ELF file.
          */
-        bool valid() const {
-            return !!m;
+        bool valid() const
+        {
+                return !!m;
         }
 
         /**
@@ -256,7 +252,6 @@ ELFPP_BEGIN_NAMESPACE
          * Return this section's name.
          */
         const char *get_name(size_t *len_out) const;
-
         /**
          * Return this section's name.  The returned string copies its
          * data, so loader liveness requirements don't apply.
@@ -268,7 +263,6 @@ ELFPP_BEGIN_NAMESPACE
          * return nullptr.
          */
         const void *data() const;
-
         /**
          * Return the size of this section in bytes.
          */
@@ -288,10 +282,10 @@ ELFPP_BEGIN_NAMESPACE
          */
         symtab as_symtab() const;
 
-    private:
+private:
         struct impl;
-        std::shared_ptr <impl> m;
-    };
+        std::shared_ptr<impl> m;
+};
 
 /**
  * A string table.
@@ -299,19 +293,20 @@ ELFPP_BEGIN_NAMESPACE
  * This class is internally reference counted and efficiently
  * copyable.
  */
-    class strtab {
-    public:
+class strtab
+{
+public:
         /**
          * Construct a strtab that is initially not valid.  Calling
          * methods other than operator= and valid on this results in
          * undefined behavior.
          */
         strtab() = default;
-
         strtab(elf f, const void *data, size_t size);
 
-        bool valid() const {
-            return !!m;
+        bool valid() const
+        {
+                return !!m;
         }
 
         /**
@@ -322,32 +317,33 @@ ELFPP_BEGIN_NAMESPACE
          * verifies that the returned string is NUL-terminated.
          */
         const char *get(Elf64::Off offset, size_t *len_out) const;
-
         /**
          * Return the string at the given offset in this string table.
          */
         std::string get(Elf64::Off offset) const;
 
-    private:
+private:
         struct impl;
-        std::shared_ptr <impl> m;
-    };
+        std::shared_ptr<impl> m;
+};
 
 /**
  * A symbol from a symbol table.
  */
-    class sym {
+class sym
+{
         const strtab strs;
         Sym<> data;
 
-    public:
+public:
         sym(elf f, const void *data, strtab strs);
 
         /**
          * Return this symbol's raw data.
          */
-        const Sym<> &get_data() const {
-            return data;
+        const Sym<> &get_data() const
+        {
+                return data;
         }
 
         /**
@@ -363,7 +359,7 @@ ELFPP_BEGIN_NAMESPACE
          * Return this symbol's name as a string.
          */
         std::string get_name() const;
-    };
+};
 
 /**
  * A symbol table.
@@ -371,63 +367,71 @@ ELFPP_BEGIN_NAMESPACE
  * This class is internally reference counted and efficiently
  * copyable.
  */
-    class symtab {
-    public:
+class symtab
+{
+public:
         /**
          * Construct a symtab that is initially not valid.  Calling
          * methods other than operator= and valid on this results in
          * undefined behavior.
          */
         symtab() = default;
-
         symtab(elf f, const void *data, size_t size, strtab strs);
 
-        bool valid() const {
-            return !!m;
+        bool valid() const
+        {
+                return !!m;
         }
 
-        class iterator {
-            const elf f;
-            const strtab strs;
-            const char *pos;
-            size_t stride;
+        class iterator
+        {
+                const elf f;
+                const strtab strs;
+                const char *pos;
+                size_t stride;
 
-            iterator(const symtab &tab, const char *pos);
-
-            friend class symtab;
+                iterator(const symtab &tab, const char *pos);
+                friend class symtab;
 
         public:
-            sym operator*() const {
-                return sym(f, pos, strs);
-            }
+                sym operator*() const
+                {
+                        return sym(f, pos, strs);
+                }
 
-            iterator &operator++() {
-                return *this += 1;
-            }
+                iterator& operator++()
+                {
+                        return *this += 1;
+                }
 
-            iterator operator++(int) {
-                iterator cur(*this);
-                *this += 1;
-                return cur;
-            }
+                iterator operator++(int)
+                {
+                        iterator cur(*this);
+                        *this += 1;
+                        return cur;
+                }
 
-            iterator &operator+=(std::ptrdiff_t x) {
-                pos += x * stride;
-                return *this;
-            }
+                iterator& operator+=(std::ptrdiff_t x)
+                {
+                        pos += x * stride;
+                        return *this;
+                }
 
-            iterator &operator-=(std::ptrdiff_t x) {
-                pos -= x * stride;
-                return *this;
-            }
+                iterator& operator-=(std::ptrdiff_t x)
+                {
+                        pos -= x * stride;
+                        return *this;
+                }
 
-            bool operator==(iterator &o) const {
-                return pos == o.pos;
-            }
+                bool operator==(iterator &o) const
+                {
+                        return pos == o.pos;
+                }
 
-            bool operator!=(iterator &o) const {
-                return pos != o.pos;
-            }
+                bool operator!=(iterator &o) const
+                {
+                        return pos != o.pos;
+                }
         };
 
         /**
@@ -440,10 +444,10 @@ ELFPP_BEGIN_NAMESPACE
          */
         iterator end() const;
 
-    private:
+private:
         struct impl;
-        std::shared_ptr <impl> m;
-    };
+        std::shared_ptr<impl> m;
+};
 
 ELFPP_END_NAMESPACE
 
